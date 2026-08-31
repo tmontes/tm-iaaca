5d7596fb5a9fb957b4bb53d77f5579ae9d52d746
Tiago Montes <self@tmont.es>
2026-08-31 20:36:58 +0100

    Fixed TLS issue cert: now uses public key name as subject name.

--- 04-tls/issue_server_certificate.py
+++ 04-tls/issue_server_certificate.py
@@ -19,12 +19,12 @@ def cli_args(command=sys.argv[0], args=sys.argv[1:]):
 if __name__ == '__main__':
 
     hostname, public_key_filename, ca_private_key_filename, ca_key_password = cli_args()
+    holder_name = x509_io.key_name(public_key_filename, name_suffix='-public')
     public_key = rsa_io.load_public_key(public_key_filename)
     ca_private_key = rsa_io.load_private_key(ca_private_key_filename, ca_key_password)
     ca_certificate = ca_io.load_ca_certificate(ca_private_key_filename)
     certificate = ca_compute.issue_certificate(
-        ca_private_key, ca_certificate, hostname, public_key, dns_names=(hostname,)
+        ca_private_key, ca_certificate, holder_name, public_key, dns_names=(hostname,)
     )
     ca_name = x509_io.key_name(ca_private_key_filename)
-    holder_name = x509_io.key_name(public_key_filename, name_suffix='-public')
     x509_io.save_certificate(certificate, f'{ca_name}-{holder_name}-{hostname}')
